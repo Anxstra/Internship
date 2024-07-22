55d0710e51e5e35b3534f93cd39206219832bf0b
Anton <kozichev2020@gmail.com>
2024-07-22 14:00:20 +0300

    update migration for proper work of hibernate hi/lo

--- tasks/task5-db-migration/security-migration/src/main/resources/security/db/changelog/scripts/populate_data.sql
+++ tasks/task5-db-migration/security-migration/src/main/resources/security/db/changelog/scripts/populate_data.sql
@@ -2,7 +2,7 @@ INSERT INTO users(id, email, password, status, version)
 VALUES (101, 'kozichev2020@gmail.com', '$2a$10$bQGMrqmYNP2gLdV.SJDq8OQh4HmRdg7ydBAXquLNgrRUSf/lE7mwK', 'ACTIVE', 0),
        (102, 'kozichev2004@gmail.com', '$2a$10$jZQVBUWSanOLM4vaWL/jMOYmHiLPy5ly2VrFGmT/ZyLUm/sgNCe22', 'ACTIVE', 0);
 
-ALTER SEQUENCE users_id_seq RESTART WITH 103;
+ALTER SEQUENCE users_id_seq RESTART WITH 127;
 
 INSERT INTO roles(id, name, version)
 VALUES (101, 'ADMIN', 0),
